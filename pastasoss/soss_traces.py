c0a70cbc64bb44082962b18ed27c1b6411f169c6
Tyler Baines <tbaines@stsci.edu>
2023-05-30 10:33:00 -0400

    updated soss_traces to pep8 standards

--- pastasoss/soss_traces.py
+++ pastasoss/soss_traces.py
@@ -1,33 +1,27 @@
-# module to predict SOSS trace positions for a given spectral order(s) 
+# Module to predict SOSS trace positions for a given spectral order(s)
 # given the  a pupil wheel position angle taken from the "PWCPOS" fits
-# header keyword. 
+# header keyword.
 
-#/Users/tbaines/miniconda3/envs/pasta-soss-3.9/lib/python3.9/site-packages
-
-from typing import  Tuple, Union
+from typing import Tuple
 
 import numpy as np
-import os
 from pkg_resources import resource_filename
 
-from pathlib import Path
+PWCPOS_CMD = 245.7600  # Commanded PWCPOS for the GR700XD
 
-# DATADIR_2 = Path(__file__) / 'data'
-
-# __all__ = ['rotate', 'get_reference_traces_positions', 'get_trace_from_reference_transform']
-
-PWCPOS_CMD = 245.7600  # reference commanded position angle for the GR700XD
-
-# DATADIR = resource_filename('pastasoss', 'data')
-# DATADIR_3 = resource_filename(__name__, 'data')
-
-# order 3 currently unsupport ATM. Will be support in the future: TBD
 REFERENCE_TRACE_FILES = {
-    'order1': resource_filename(__name__, 'data/jwst_niriss_gr700xd_order1_trace_refmodel.txt'),
-    'order2': resource_filename(__name__, 'data/jwst_niriss_gr700xd_order2_trace_refmodel.txt'),
+    "order1": "jwst_niriss_gr700xd_order1_trace_refmodel.txt",
+    "order2": "jwst_niriss_gr700xd_order2_trace_refmodel.txt",
+    # order 3 currently unsupport ATM. Will be support in the future: TBD
 }
 
-def rotate(x: np.ndarray, y: np.ndarray, angle: float, origin: Tuple[float, float]=(0, 0), interp: bool=True) -> Tuple[np.ndarray, np.ndarray]:
+
+def rotate(
+    x: np.ndarray,
+    y: np.ndarray,
+    angle: float,
+    origin: Tuple[float, float] = (0, 0),
+    interp: bool = True) -> Tuple[np.ndarray, np.ndarray]:
     """
     Applies a rotation transformation to a set of 2D points.
 
@@ -42,7 +36,8 @@ def rotate(x: np.ndarray, y: np.ndarray, angle: float, origin: Tuple[float, floa
     origin : Tuple[float, float], optional
         The point about which to rotate the points. Default is (0, 0).
     interp : bool, optional
-        Whether to interpolate the rotated positions onto the original x-pixel column values. Default is True.
+        Whether to interpolate the rotated positions onto the original x-pixel
+        column values. Default is True.
 
     Returns
     -------
@@ -55,31 +50,30 @@ def rotate(x: np.ndarray, y: np.ndarray, angle: float, origin: Tuple[float, floa
     >>> y = np.array([0, 1, 2, 3])
     >>> x_rot, y_rot = rotate(x, y, 90)
     """
-    # shift to rotate about center    
+
+    # shift to rotate about center
     xy_center = np.atleast_2d(origin).T
-    xy = np.vstack([x, y]) 
-    
+    xy = np.vstack([x, y])
+
     # Rotation transform matrix
     radians = np.radians(angle)
     c, s = np.cos(radians), np.sin(radians)
-    R = np.array([[c, -s], 
-                  [s,  c]])
-    
+    R = np.array([[c, -s], [s, c]])
+
     # apply transformation
     x_new, y_new = R @ (xy - xy_center) + xy_center
-    
-    # interpolate rotated positions onto x-pixel column values (default)
 
+    # interpolate rotated positions onto x-pixel column values (default)
     if interp:
         y_new = np.interp(x, x_new, y_new)
-        return x, y_new
-    else:
-        return x_new, y_new
-    
 
-def get_reference_traces_positions(file: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    return x_new, y_new
+
+
+def get_reference_trace(file: str) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    Load in the reference trace positions given a file associated with a given spectral order.
+    Load in the reference trace positions given a file associated with a given
+    spectral order.
 
     Parameters
     ----------
@@ -89,49 +83,53 @@ def get_reference_traces_positions(file: str) -> Tuple[np.ndarray, np.ndarray, n
     Returns
     -------
     Tuple[np.ndarray, np.ndarray, np.ndarray]
-        A tuple containing the x, y positions and origin of the reference traces.
-
-    Raises
-    ------
-    IOError
-        If the file cannot be found or read.
+        A tuple containing the x, y positions and origin of the reference
+        traces.
 
     Examples
     --------
-    >>> x, y, origin = get_reference_traces_positions('reference_trace_filename.txt')
+    >>> x, y, origin = get_reference_traces_positions('ref_filename.txt')
     """
-    traces = np.loadtxt(file)
+    filepath = resource_filename(__name__, f"data/{file}")
+    traces = np.loadtxt(filepath)
     origin = traces[0]
-    x = traces[1:,0]
-    y = traces[1:,1]
+    x = traces[1:, 0]
+    y = traces[1:, 1]
     return x, y, origin
 
 
-def get_trace_from_reference_transform(pwcpos: float, order: str='123', interp: bool=True) -> Union[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+def get_soss_traces(pwcpos: float, order: str = "123", interp: bool = True) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Gets gr700xd trace position given the pupil wheel positions angle provided in the FITS header under keyword PWCPOS. 
-    Traces for a given spectral order are predict by perform a rotation transformation on the reference trace positions.
-    This methods yield sub-pixel performance and will be improved upon in later interations as more NIRISS/SOSS observations
-    become available. 
+    This is the primary method for generate the gr700xd trace position given a
+    pupil wheel positions angle provided in the FITS header under keyword
+    PWCPOS. The traces for a given spectral order are found by perform a
+    rotation transformation using the refence trace positions at the commanded
+    PWCPOS=245.76 degrees. This methods yield sub-pixel performance and will be
+    improved upon in later interations as more NIRISS/SOSS observations become
+    available.
 
     Parameters
     ----------
     pwcpos : float
-        The pupil wheel positions angle provided in the FITS header under keyword PWCPOS.
+        The pupil wheel positions angle provided in the FITS header under
+        keyword PWCPOS.
     order : str, optional
-        The spectral order to compute the new traces for. Default is '123'. 
+        The spectral order to compute the new traces for. Default is '123'.
         Support for order 3 will be added at a later date.
     interp : bool, optional
-        Whether to interpolate the rotated positions onto the original x-pixel column values. 
-        Default is True.
+        Whether to interpolate the rotated positions onto the original x-pixel
+        column values. Default is True.
 
     Returns
     -------
-    Union[Tuple[np.ndarray, np.ndarray], List[Tuple[np.ndarray, np.ndarray]]]
-        If `order` is '1', a tuple of the x and y coordinates of the rotated points for the first spectral order.
-        If `order` is '2', a tuple of the x and y coordinates of the rotated points for the second spectral order.
-        If `order` is '3' or a combination of '1', '2', and '3', a list of tuples of the x and y coordinates of the 
-        rotated points for each spectral order.
+    Tuple[np.ndarray, np.ndarray]]
+    If `order` is '1', a tuple of the x and y coordinates of the rotated
+    points for the first spectral order.
+    If `order` is '2', a tuple of the x and y coordinates of the rotated
+    points for the second spectral order.
+    If `order` is '3' or a combination of '1', '2', and '3', a list of
+    tuples of the x and y coordinates of the rotated points for each
+    spectral order.
 
     Raises
     ------
@@ -142,27 +140,30 @@ def get_trace_from_reference_transform(pwcpos: float, order: str='123', interp: 
     --------
     >>> x_new, y_new = get_trace_from_reference_transform(2.3)
     """
-    
+
     norders = len(order)
-    
+
     if norders > 3:
-        raise ValueError('order must be: 1, 2, 3, or combination, Example of 123')
-   
+        raise ValueError("order must be: 1,2, 3.")
     if norders > 1:
         # recursively compute the new traces for each order
-        return [get_trace_from_reference_transform(pwcpos, odr) for odr in order]
-    
-    elif order == '1' :
-        x, y, origin = get_reference_traces_positions(REFERENCE_TRACE_FILES['order1'])
+        return [get_soss_traces(pwcpos, m) for m in order]
 
-    elif order == '2':
-        x, y, origin = get_reference_traces_positions(REFERENCE_TRACE_FILES['order2'])
+    # This might be an alternative way of writing this
+    # if 'order'+order in REFERENCE_TRACE_FILES.keys():
+    #     ref_file = REFERENCE_TRACE_FILES["order"+order]
+    elif order == "1":
+        ref_file = REFERENCE_TRACE_FILES["order1"]
 
-    elif order == '3':
-        print('Order 3 is not yet supported as of yet. Will be supported in the future')
-        return 
-       
-    x_new, y_new = rotate(x, y, pwcpos-PWCPOS_CMD, origin, interp=interp)
+    elif order == "2":
+        ref_file = REFERENCE_TRACE_FILES["order2"]
 
-    return x_new, y_new 
+    elif order == "3":
+        print("The software currently does not support order 3 at this time.")
+        return None
 
+    x, y, origin = get_reference_trace(ref_file)
+
+    x_new, y_new = rotate(x, y, pwcpos - PWCPOS_CMD, origin, interp=interp)
+
+    return x_new, y_new
